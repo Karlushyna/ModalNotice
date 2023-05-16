3cb1b43057a639e81543c82a50814820a108cc8f
Karlushyna Lesya <karlushyna.l@gmail.com>
2023-05-17 00:39:51 +0100

    upd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ParentComponent from "Components/Modal/ParentComponent";
+
+function App() {
+
+
+  return (
+    <>
+      <ParentComponent />
+    </>
+  );
+}
+
+export default App;
